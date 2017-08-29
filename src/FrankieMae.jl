8b33bcfb01de25248f7268c0ca241f887c0ae3b6
alejandrorbraun <31396210+alejandrorbraun@users.noreply.github.com>
2017-08-28 22:04:08 -0500

    a

--- src/FrankieMae.jl
+++ src/FrankieMae.jl
@@ -7,6 +7,9 @@ import Dates
 
 # ReadFiles
 fmopen(parentpath,startrange,endrange) = begin
- Pandas.read_csv(
- join(parentpath,header=-1,delimiter="|")
+## for daterange, create an array where 1:4 is repeated endrange-(startrange+1) and with startrange quarter:4 appended to front and 1:endrange quarter is appended to the end
+ daterange =
+
+[Pandas.read_csv(
+ join(parentpath, daterange[n]),header=-1,delimiter="|") for n=1:length(daterange)]
 end # module
